d220ca8a0e7e785e74d92739f4311fbd7b5fbfa7
TripleYei <58819156+TripleYei@users.noreply.github.com>
2022-10-07 22:10:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,3 +14,5 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
    console.log("Service worker activated");
 });
+
+ self.addEventListener('fetch',() => console.log("fetch"));
