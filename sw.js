0cb255cb28c19b1f51bf2455c645ce87fca9b6c5
TripleYei <58819156+TripleYei@users.noreply.github.com>
2022-10-07 22:05:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
+
+let CACHE_NAME = 'my-cache';
+let urlsToCache = [
+    'img/apple-touch-icon.png',    
+    'img/favicon-32x32.png',
+    'img/favicon-16x16.png',
+    'sw.js'
+
+    ];
+
 self.addEventListener("install", event => {
    console.log("Service worker installed");
 });
